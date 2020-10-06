41658c26902746b1e72d5f72ed36c57ab4b24df0
Benjamin Griffiths <drjamin1990@gmail.com>
2020-10-06 11:05:42 +0100

    testing switcher

--- cloud-switch/switcher.py
+++ cloud-switch/switcher.py
@@ -1,7 +1,18 @@
 import cloudsync
 
-prov = cloudsync.create_provider("onedrive")
-creds = prov.authenticate()
-prov.connect(creds)
-with open("/my/local/file", "wb") as f:
-    prov.download_path("/path/to/file/on/onedrive", f):
+# Get a generic client_id and client_secret. Do not use this in production code.
+# For more information on getting your own client_id and client_secret, see README_OAUTH.md
+oauth_config = cloudsync.command.utils.generic_oauth_config('gdrive')
+
+# get an instance of the gdrive provider class
+provider = cloudsync.create_provider('gdrive', oauth_config=oauth_config)
+
+# Start the oauth process to login in to the cloud provider
+creds = provider.authenticate()
+
+# Use the credentials to connect to the cloud provider
+provider.connect(creds)
+
+# Perform cloud operations
+for entry in provider.listdir_path("/"):
+    print(entry.path)
